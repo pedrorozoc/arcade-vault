3a3a04d7b9c8c9911569cd9f781543036e756ee9
Pedro Rozo <prozoc@dian.gov.co>
2026-09-08 10:31:24 -0500

    Paso 5: Añadir importación del componente AsteroidsGame y ajustar la función JugarPage para renderizarlo

--- app/juego/[id]/jugar/page.tsx
+++ app/juego/[id]/jugar/page.tsx
@@ -1,11 +1,15 @@
 import { notFound } from "next/navigation";
 import { GAMES } from "@/lib/data";
 import GamePlayer from "@/components/GamePlayer";
+import AsteroidsGame from "@/components/games/AsteroidsGame";
 
-export default async function JugarPage({ params }: PageProps<"/juego/[id]/jugar">) {
+export default async function JugarPage({
+  params,
+}: PageProps<"/juego/[id]/jugar">) {
   const { id } = await params;
   const game = GAMES.find((g) => g.id === id);
   if (!game) notFound();
 
+  if (id === "asteroides") return <AsteroidsGame game={game} />;
   return <GamePlayer game={game} />;
 }
